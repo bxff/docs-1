435dec58b622b0d0ba3575d34200eda9abf37854
rishabhpoddar <rishabh.poddar@gmail.com>
2022-09-27 13:53:30 +0530

    sets up ios env - initial attempt

---
+++ v2/src/plugins/codeTypeChecking/iosEnv/application/application/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  application
+//
+//  Created by Rishabh Poddar on 27/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ v2/src/plugins/codeTypeChecking/iosEnv/application/application/applicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  applicationApp.swift
+//  application
+//
+//  Created by Rishabh Poddar on 27/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct applicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
